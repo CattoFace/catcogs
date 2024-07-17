77bfe893c16023a8bc89c85b0db7d28d6bb56932
Barr <barr@barrisrael.com>
2024-07-17 22:32:14 +0300

    try eomjis

--- mapleutil/mapleutil.py
+++ mapleutil/mapleutil.py
@@ -259,7 +259,7 @@ class MapleUtil(commands.Cog):
         emoji_map = {'<:img:1263215863453192204>':4}
         async def selectworld(ctx, pages, controls, message, page, timeout, emoji):
             print(emoji)
-            await menu(ctx, pages, controls, message=message, page=emoji_map[emoji], timeout=timeout)
+            return await menu(ctx, pages, controls, message=message, page=emoji_map[emoji], timeout=timeout)
         controls = {'<:img:1263215863453192204>':selectworld}
         await menu(ctx,embeds, controls)
 
