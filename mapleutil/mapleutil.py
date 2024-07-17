fb04c3edb59e926b08bfffaecec02018a04d3bf2
Barr <barr@barrisrael.com>
2024-07-17 23:23:38 +0300

    try eomjis

--- mapleutil/mapleutil.py
+++ mapleutil/mapleutil.py
@@ -257,12 +257,10 @@ class MapleUtil(commands.Cog):
         ctx = await self.bot.get_context(interaction)
         menu_items, start_index = scrapelib.fetchServerStatus(self.session, world)
         embeds = [generateEmbed(n,c) for (n,c) in menu_items]
-        if world is not None:
-            menu = SimpleMenu(embeds, start_index, use_select_menu=True)
-        else:
-            menu = SimpleMenu(embeds, use_select_menu=True)
+        menu = SimpleMenu(embeds, start_index, use_select_menu=True)
         # hack to get custom page names
         menu.select_options = [
             discord.SelectOption(label=n, value=num) for num, (n,_) in enumerate(menu_items)
         ]
+        menu.select_menu = menu._get_select_menu()
         await menu.start(ctx)
