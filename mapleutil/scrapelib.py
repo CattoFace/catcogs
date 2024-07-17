5ffcd5aaba3d2d27fb6c7365494717c665a2ecf1
Barr <barr@barrisrael.com>
2024-07-17 17:47:14 +0300

    work on menu

--- mapleutil/scrapelib.py
+++ mapleutil/scrapelib.py
@@ -6,7 +6,8 @@ def status_dict2str(status):
         output += f"Login {i+1}: {'UP' if status[f'login_0{i}']==1 else 'DOWN'}"
     output += "\n"
     for i in range(40):
-        output += f"Channel {i+1}: {'UP' if status['login_{"""0""" if i<10 else """"""}{i}']==1 else 'DOWN'}"
+        label = f"login_{'0' if i<10 else ''}{i}"
+        output += f"Channel {i+1}: {'UP' if status[label]==1 else 'DOWN'}"
         if i%5==0:
             output+="\n"
 
