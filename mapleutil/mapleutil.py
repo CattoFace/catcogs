e01f3988212c4aaeecd4638f3f15a08b7ee05be3
Barr <barr@barrisrael.com>
2024-07-17 23:10:36 +0300

    try eomjis

--- mapleutil/mapleutil.py
+++ mapleutil/mapleutil.py
@@ -258,4 +258,7 @@ class MapleUtil(commands.Cog):
         menu_items, start_index = scrapelib.fetchServerStatus(self.session, world)
         print(start_index)
         embeds = {n:generateEmbed(n,c) for (n,c) in menu_items}
-        await SimpleMenu(embeds, start_index, use_select_menu=True).start(ctx)
+        if world is not None:
+            await SimpleMenu(embeds, world, use_select_menu=True).start(ctx)
+        else:
+            await SimpleMenu(embeds, use_select_menu=True).start(ctx)
